1b2ddfa8825eb0156ead79507d588e7b6ddaac7a
Tina <longevity.tina@gmail.com>
2020-10-20 12:38:07 -0700

    successfully completes call to gsheet

---
+++ index.js
@@ -0,0 +1,13 @@
+var GoogleSpreadsheet = require("google-spreadsheet");
+var creds = require("./client_secret.json");
+
+// Create a document object using the ID of the spreadsheet - obtained from its URL.
+var doc = new GoogleSpreadsheet("1oTl_IPN_ILeoivP5JVFnj1H1CDTZ9adIIf1FBKTuWKM");
+
+// Authenticate with the Google Spreadsheets API.
+doc.useServiceAccountAuth(creds, function (err) {
+  // Get all of the rows from the spreadsheet.
+  doc.getRows(3, function (err, rows) {
+    console.log(rows);
+  });
+});
