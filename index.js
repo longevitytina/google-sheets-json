778440a438767d0d7f338180df5482d5199dd687
Tina <longevity.tina@gmail.com>
2020-10-23 14:26:15 -0700

    small edit

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ var creds = require("./client_secret.json");
 
 // Create a document object using the ID of the spreadsheet - obtained from its URL.
 var doc = new GoogleSpreadsheet("1oTl_IPN_ILeoivP5JVFnj1H1CDTZ9adIIf1FBKTuWKM");
-
+//
 // Authenticate with the Google Spreadsheets API.
 doc.useServiceAccountAuth(creds, function (err) {
   // Get all of the rows from the spreadsheet.
